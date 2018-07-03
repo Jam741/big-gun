76b35a0d18d55898b1182ae7cea3ac200635ef02
hejiaming <jamisonline.he@gmail.com>
2018-07-03 10:37:21 +0800

    add jpa depednency

---
+++ src/main/java/com/jam/biggun/domain/UserEntity.java
@@ -0,0 +1,10 @@
+package com.jam.biggun.domain;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import java.io.Serializable;
+
+
+@EntityScan
+public class UserEntity  implements Serializable {
+}
